9d1981be378430011a83e818a0d4c994c43cf0d3
phlaster <phlaster@users.noreply.github.com>
2024-07-08 22:16:23 +0300

    working camera with styles

--- components/Qr_screen.tsx
+++ components/Qr_screen.tsx
@@ -1,34 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { Camera } from 'expo-camera';
-import { Button } from 'react-native-paper';
+import React, { useState, useEffect } from "react";
+import { Dimensions, Alert, Vibration, Pressable, View } from "react-native";
+import { Button, Text, Surface } from 'react-native-paper';
+import { Camera, CameraView } from 'expo-camera';
+import { router } from "expo-router";
+import * as Linking from "expo-linking";
 import styles from '../styles/style';
 
 function QRScanner({ navigation }) {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasPermissions, setHasPermissions] = useState(false);
+  const [isCameraFrozen, setIsCameraFrozen] = useState(false);
+  const [scannedData, setScannedData] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      setHasPermissions(cameraStatus === "granted" );
     })();
   }, []);
 
-  if (hasPermission === null) {
-    return <View><Text>Requesting camera permission</Text></View>;
-  }
-  if (hasPermission === false) {
-    return <View><Text>No access to camera</Text></View>;
+  useEffect(() => {
+    if (hasPermissions === false) {
+      Alert.alert(
+        "Camera Permissions Required",
+        "You must grant access to your camera to scan QR codes",
+        [
+          { text: "Go to settings", onPress: () => Linking.openSettings() },
+          { text: "Cancel", onPress: () => router.dismissAll(), style: "cancel" },
+        ]
+      );
+    }
+  }, [hasPermissions]);
+
+  const handleBarCodeScanned = ({ data }) => {
+    Vibration.vibrate([0, 70]);
+    setScannedData(data);
+    setIsCameraFrozen(true);
+  };
+
+  const handleScanAgain = () => {
+    setScannedData(null);
+    setIsCameraFrozen(false);
+  };
+
+  const handleProceed = () => {
+    navigation.navigate('After_Scan', { data: scannedData });
+  };
+
+  if (!hasPermissions) {
+    return null;
   }
 
   return (
-    <View style={styles.container}>
-      <Text>QR Scanner</Text>
-      <Button mode="contained" onPress={() => navigation.goBack()}>
-        Go Back
-      </Button>
+<View style={styles.cameraContainer}>
+      {isCameraFrozen && (
+        <View style={styles.qrContentBox}>
+          <Text style={styles.qrContentText}>{scannedData}</Text>
+        </View>
+      )}
+      <CameraView
+        onBarcodeScanned={isCameraFrozen ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+        style={styles.cameraView}
+      />
+      <View style={styles.buttonContainer}>
+        <Button style={[styles.buttonCamera, styles.cancelButton]} onPress={() => navigation.navigate('LK')}>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </Button>
+        <Button 
+          style={[styles.buttonCamera, isCameraFrozen ? {} : styles.disabledButton]} 
+          onPress={handleScanAgain} 
+          disabled={!isCameraFrozen}
+        >
+          <Text style={styles.buttonText}>Scan again</Text>
+        </Button>
+        <Button 
+          style={[styles.buttonCamera, isCameraFrozen ? {} : styles.disabledButton]} 
+          onPress={handleProceed} 
+          disabled={!isCameraFrozen}
+        >
+          <Text style={styles.buttonText}>Proceed</Text>
+        </Button>
+      </View>
     </View>
   );
 }
-
 export default QRScanner;
--- styles/style.js
+++ styles/style.js
@@ -4,219 +4,255 @@ import { Dimensions } from 'react-native';
 
 
 const theme = {
-    ...DefaultTheme,
-    colors: {
-        ...DefaultTheme.colors,
-        primary: '#294985FF',
-        accent: '#03dac4',
-        background: '#F6F5F4FF',
-        text: '#2C2C2CFF'
-    },
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#294985FF',
+    accent: '#03dac4',
+    background: '#F6F5F4FF',
+    text: '#2C2C2CFF',
+    red: '#9f0e0e',
+  },
 };
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: theme.colors.background,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 16,
-    },
-    authSurface: {
-        padding: 20,
-        borderRadius: 10,
-        elevation: 4,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-        textAlign: 'center',
-        color: theme.colors.primary,
-    },
-    input: {
-      fontSize: 18,
-        marginBottom: 16,
-        width: 200
-        
-    },
-    button: {
-        marginTop: 8,
-        paddingVertical: 8,
-        color: theme.colors.primary,
-    },
-    textButton: {
-        marginTop: 16,
-    },
-    mainSurface: {
-        padding: 24,
-        borderRadius: 8,
-        elevation: 4,
-        alignItems: 'center',
-        backgroundColor: 'white',
-        width: '100%',
-        maxWidth: 400,
-    },
-    logo: {
-        width: 120,
-        height: 120,
-        marginBottom: 16,
-    },
-    mainTitle: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: theme.colors.primary,
-        marginBottom: 16,
-    },
-    mainText: {
-        fontSize: 16,
-        textAlign: 'center',
-        marginBottom: 24,
-        color: theme.colors.text,
-    },
-    mainButton: {
-        width: '100%',
-        marginTop: 8,
-        color: theme.colors.primary,
-    },
-    mainButtonContent: {
-        height: 48,
-        flexDirection: 'row-reverse',
-    },
-    scrollContainer: {
-        flexGrow: 1,
-        backgroundColor: theme.colors.background,
-        padding: 16,
-      },
-      profileSurface: {
-        padding: 24,
-        borderRadius: 8,
-        elevation: 4,
-        alignItems: 'center',
-        backgroundColor: 'white',
-        marginBottom: 16,
-      },
-      userInfo: {
-        alignItems: 'center',
-        marginTop: 16,
-      },
-      userName: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: theme.colors.primary,
-      },
-      userEmail: {
-        fontSize: 14,
-        color: theme.colors.text,
-      },
-      logoutButton: {
-        marginTop: 16,
-      },
-      buttonGrid: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        marginBottom: 16,
-      },
-      gridButton: {
-        width: '48%',
-        marginBottom: 16,
-      },
-      statsSurface: {
-        padding: 24,
-        borderRadius: 8,
-        elevation: 4,
-        backgroundColor: 'white',
-      },
-      statsTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 16,
-        color: theme.colors.primary,
-      },
-      statsGrid: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-      },
-      statItem: {
-        width: '48%',
-        alignItems: 'center',
-        marginBottom: 16,
-      },
-      statValue: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: theme.colors.primary,
-      },
-      statLabel: {
-        fontSize: 14,
-        color: theme.colors.text,
-      },
-      statusBar: {
-        height: '20%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: 'white',
-        paddingHorizontal: 10,
-        paddingTop: 16,
-        paddingBottom: 16,
-      },
-      statusBarUserInfo: {
-        flex: 1,
-        marginLeft: 16,
-      },
-      statusBarUsername: {
-        fontSize: 18,
-        fontWeight: 'bold',
-      },
-      statusBarEmail: {
-        fontSize: 14,
-        color: theme.colors.placeholder,
-      },
-      statusBarLogoutButton: {
-        // margin: -10,
-        // padding: -5,
-        borderRadius: 15,
-        backgroundColor: '#9f0e0e',
-      },
-      statusBarLogoutButtonLabel: {
-        fontSize: 16,
-        color: '#ffffff',
-        marginHorizontal: 8
-      },
-      connectButton: {
-        backgroundColor: '#808080',
-      },
-      connectButtonLabel: {
-        color: '#000000',
-      },
-      logoutButton: {
-        backgroundColor: '#9f0e0e',
-      },
-      logoutButtonLabel: {
-        color: '#ffffff',
-      },
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  authSurface: {
+    padding: 20,
+    borderRadius: 10,
+    elevation: 4,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+    color: theme.colors.primary,
+  },
+  input: {
+    fontSize: 18,
+    marginBottom: 16,
+    width: 200
 
+  },
+  button: {
+    marginTop: 8,
+    paddingVertical: 8,
+    color: theme.colors.primary,
+  },
+  textButton: {
+    marginTop: 16,
+  },
+  mainSurface: {
+    padding: 24,
+    borderRadius: 8,
+    elevation: 4,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: '100%',
+    maxWidth: 400,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 16,
+  },
+  mainTitle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+    marginBottom: 16,
+  },
+  mainText: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 24,
+    color: theme.colors.text,
+  },
+  mainButton: {
+    width: '100%',
+    marginTop: 8,
+    color: theme.colors.primary,
+  },
+  mainButtonContent: {
+    height: 48,
+    flexDirection: 'row-reverse',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    backgroundColor: theme.colors.background,
+    padding: 16,
+  },
+  profileSurface: {
+    padding: 24,
+    borderRadius: 8,
+    elevation: 4,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginBottom: 16,
+  },
+  userInfo: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+  userEmail: {
+    fontSize: 14,
+    color: theme.colors.text,
+  },
+  logoutButton: {
+    marginTop: 16,
+  },
+  buttonGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  gridButton: {
+    width: '48%',
+    marginBottom: 16,
+  },
+  statsSurface: {
+    padding: 24,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: 'white',
+  },
+  statsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: theme.colors.primary,
+  },
+  statsGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  statItem: {
+    width: '48%',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  statValue: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+  statLabel: {
+    fontSize: 14,
+    color: theme.colors.text,
+  },
+  statusBar: {
+    height: '20%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  statusBarUserInfo: {
+    flex: 1,
+    marginLeft: 16,
+  },
+  statusBarUsername: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  statusBarEmail: {
+    fontSize: 14,
+    color: theme.colors.placeholder,
+  },
+  statusBarLogoutButton: {
+    // margin: -10,
+    // padding: -5,
+    borderRadius: 15,
+    backgroundColor: theme.colors.red,
+  },
+  statusBarLogoutButtonLabel: {
+    fontSize: 16,
+    color: theme.colors.background,
+    marginHorizontal: 8
+  },
+  connectButton: {
+    backgroundColor: '#808080',
+  },
+  connectButtonLabel: {
+    color: theme.colors.text,
+  },
+  buttonLabel: {
+    color: theme.colors.text,
+  },
+  logoutButton: {
+    backgroundColor: theme.colors.red,
+  },
+  logoutButtonLabel: {
+    color: '#ffffff',
+  },
 
-      cameraSurface: {
-        width: '100%',
-        height: Dimensions.get('window').height * 0.7,
-        overflow: 'hidden',
-        borderRadius: 8,
-        marginBottom: 16,
-      },
-      camera: {
-        flex: 1,
-      },
-      buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-      },
-      button: {
-        flex: 1,
-        marginHorizontal: 4,
-      },
+  cameraContainer: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  qrContentBox: {
+    position: 'absolute',
+    top: '10%',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 16,
+    elevation: 4,
+    zIndex: 1,
+  },
+  qrContentText: {
+    fontSize: 18,
+    color: theme.colors.primary,
+    textAlign: 'center',
+  },
+  cameraView: {
+    width: '100%',
+    height: '60%',
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 16,
+  },
+  buttonCamera: {
+    flex: 1,
+    marginHorizontal: 8,
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    backgroundColor: theme.colors.primary,
+  },
+  cancelButton: {
+    backgroundColor: theme.colors.red,
+  },
+  disabledButton: {
+    backgroundColor: theme.colors.background,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
